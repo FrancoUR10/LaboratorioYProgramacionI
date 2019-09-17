a856a84d0ce125a4d4e66e3984ff038a06305122
alumno <alumno>
2019-09-17 12:29:15 -0300

    clase numero 9

--- Clase_9/Practica_Clase_9/Funciones.h
+++ Clase_9/Practica_Clase_9/Funciones.h
@@ -10,6 +10,14 @@ typedef struct
 
 int getInt(char mensaje[]);
 void inicializarArray(eAlumno unArray[],int tam);
-void ordenarInsercion(char unArray[][256],int tam);
+void ordenarInsercion(eAlumno unArray[],int tam);
+void harcodearAlumnos(eAlumno unArray[]);
+int buscarSoloEstado(eAlumno unArray[],int tam,int estado);
+void mostrarUnSoloAlumno(eAlumno unAlumno[],int indice);
+void mostrarLista(eAlumno unArray[],int tam);
+void cargarDatos(eAlumno unArray[],int tam);
+void borrarDatos(eAlumno unArray[],int tam);
+int buscarLegajo(eAlumno unArray[],int tam,int legajo);
+void modificarDatos(eAlumno unArray[],int tam);
 
 #endif // FUNCIONES_H_INCLUDED
