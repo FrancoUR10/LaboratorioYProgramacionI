597989c1f3409235cce855571300f0639d51dc41
alumno <alumno>
2019-11-30 12:09:29 -0300

    clase repaso

---
+++ Clases_De_Repaso/Clase_Repaso_11/Primer_Parcial_Laboratorio_enunciados/Elenco.c
@@ -0,0 +1,352 @@
+#include "Elenco.h"
+/*
+1. Mostrar las películas de terror cuyo año de estreno sea después del 2002.
+2. Películas en donde haya participado un actor argentino.
+3. Mostrar las películas en las que participó un actor seleccionado.
+4. Seleccionar un actor y determinar cuánto ha recaudado en todas las
+   películas románticas en las que participó.
+
+5. Listar los actores que no participaron en ninguna película.
+*/
+void elenco_informarDatos(eElenco listaElenco[],int tamElenco,eActor listaActor[],int tamActor,ePelicula listaPelicula[],int tamPelicula,eGenero listaGenero[],int tamGenero,ePais listaPais[],int tamPais)
+{
+    int opcionMenu;
+    char continuarMenu='s';
+    do
+    {
+        system("cls");
+        printf("1-MOSTRAR PELICULAS DESPUES DEL 2002\n");
+        printf("2-MOSTRAR PELICULAS CON UN ACTOR ARGENTINO\n");
+        printf("3-MOSTRAR PELICULAS CON UN ACTOR SELECCIONADO\n");
+        printf("4-MOSTRAR RECAUDO TOTAL DE UN ACTOR EN PELICULAS ROMANTICAS\n");
+        printf("5-MOSTRAR ACTORES QUE NO PARTICIPAN EN PELICULAS\n");
+        printf("6-SALIR DEL SUB-MENU\n");
+        opcionMenu=getInt("\nIngrese una opcion: ");
+        switch(opcionMenu)
+        {
+            case 1:
+                pelicula_mostrarPeliculasDespuesDel2002(listaPelicula,tamPelicula,listaGenero,tamGenero);
+                system("pause");
+                break;
+            case 2:
+                elenco_mostrarPeliculasConActorArgentino(listaElenco,tamElenco,listaPelicula,tamPelicula,listaActor,tamActor,listaGenero,tamGenero,listaPais,tamPais);
+                system("pause");
+                break;
+            case 3:
+                elenco_mostrarPeliculasConUnActorSeleccionado(listaElenco,tamElenco,listaPelicula,tamPelicula,listaActor,tamActor,listaGenero,tamGenero,listaPais,tamPais);
+                system("pause");
+                break;
+            case 4:
+                system("pause");
+                break;
+            case 5:
+                elenco_mostrarActoresQueNoParticipanEnPeliculas(listaElenco,tamElenco,listaPelicula,tamPelicula,listaActor,tamActor,listaGenero,tamGenero,listaPais,tamPais);
+                system("pause");
+                break;
+            case 6:
+                continuarMenu='n';
+                break;
+            default:
+                printf("\nOpcion ingresada no valida\n");
+                system("pause");
+        }
+    }
+    while(continuarMenu=='s');
+}
+void elenco_gestionarElencos(eElenco listaElenco[],int tamElenco,eActor listaActor[],int tamActor,ePelicula listaPelicula[],int tamPelicula,eGenero listaGenero[],int tamGenero,ePais listaPais[],int tamPais,int* contAltas)
+{
+    int opcionMenu;
+    char continuarMenu='s';
+    do
+    {
+        system("cls");
+        printf("1-GENERAR ELENCOS\n");
+        printf("2-LISTAR ELENCOS\n");
+        printf("3-SALIR DEL SUB-MENU\n");
+        opcionMenu=getInt("\nIngrese una opcion: ");
+        switch(opcionMenu)
+        {
+            case 1:
+                elenco_darDeAlta(listaElenco,tamElenco,listaActor,tamActor,listaPelicula,tamPelicula,listaGenero,tamGenero,listaPais,tamPais,contAltas);
+                system("pause");
+                break;
+            case 2:
+                elenco_mostrarLista(listaElenco,tamElenco,listaActor,tamActor,listaPelicula,tamPelicula);
+                system("pause");
+                break;
+            case 3:
+                continuarMenu='n';
+                break;
+            default:
+                printf("\nOpcion ingresada no valida\n");
+                system("pause");
+        }
+    }
+    while(continuarMenu=='s');
+}
+void elenco_inicializarDatos(eElenco listaElenco[],int tamElenco)
+{
+    int i;
+    for(i=0;i<tamElenco;i++)
+    {
+        listaElenco[i].codigoDeReparto=-1;
+        listaElenco[i].codigoPelicula=-1;
+        listaElenco[i].codigoActor=-1;
+        listaElenco[i].valorContrato=-1;
+    }
+}
+void elenco_harcodearDatos(eElenco listaelenco[])
+{
+    int codigoDeReparto[5]={1,2,3,4,5};
+    int codigoPelicula[5]={3,2,3,4,5};
+    int codigoActor[5]={3,2,4,1,1};
+    float valorContrato[5]={5000,6000,8000,2000,3000};
+    int i;
+    for(i=0;i<5;i++)
+    {
+        listaelenco[i].codigoDeReparto=codigoDeReparto[i];
+        listaelenco[i].codigoPelicula=codigoPelicula[i];
+        listaelenco[i].codigoActor=codigoActor[i];
+        listaelenco[i].valorContrato=valorContrato[i];
+    }
+}
+void elenco_mostrarUnoSolo(eElenco unaelenco,eActor unActor[],int indiceActor,ePelicula unaPelicula[],int indicePelicula)
+{
+    printf("\n%d",unaelenco.codigoDeReparto);
+    printf("%30s",unaPelicula[indicePelicula].descripcion);
+    printf("%16s",unActor[indiceActor].nombre);
+    printf("%14.2f\n",unaelenco.valorContrato);
+}
+void elenco_mostrarLista(eElenco listaelenco[],int tamelenco,eActor listaActor[],int tamActor,ePelicula listaPelicula[],int tamPelicula)
+{
+    int indiceBusquedaActor;
+    int indiceBusquedaPelicula;
+    int i;
+    printf("\nCODIGO_REPARTO      PELICULA            ACTOR        VALOR_CONTRATO\n");
+    elenco_ordenarDatos(listaelenco,tamelenco,listaPelicula,tamPelicula,listaActor,tamActor);
+    for(i=0;i<tamelenco;i++)
+    {
+        indiceBusquedaActor=actor_buscarPorCodigo(listaActor,tamActor,listaelenco[i].codigoActor);
+        indiceBusquedaPelicula=pelicula_buscarPorCodigo(listaPelicula,tamPelicula,listaelenco[i].codigoPelicula);
+        if((listaActor[indiceBusquedaActor].estado==OCUPADO) && (listaelenco[i].codigoActor==listaActor[indiceBusquedaActor].codigo) && (listaelenco[i].codigoPelicula==listaPelicula[indiceBusquedaPelicula].codigo))
+        {
+            elenco_mostrarUnoSolo(listaelenco[i],listaActor,indiceBusquedaActor,listaPelicula,indiceBusquedaPelicula);
+        }
+    }
+}
+int elenco_buscarPorCodigoDeReparto(eElenco listaelenco[],int tamelenco,int codigoRepartoABuscar)
+{
+    int retorno=-1;
+    int i;
+    for(i=0;i<tamelenco;i++)
+    {
+        if(listaelenco[i].codigoDeReparto==codigoRepartoABuscar)
+        {
+            retorno=i;
+            break;
+        }
+    }
+    return retorno;
+}
+int elenco_darDeAlta(eElenco listaElenco[],int tamElenco,eActor listaActor[],int tamActor,ePelicula listaPelicula[],int tamPelicula,eGenero listaGenero[],int tamGenero,ePais listaPais[],int tamPais,int* contAltas)
+{
+    int confirmacion;
+    eElenco auxDatos;
+    int auxCodigoRepartoInt;
+    int flagIngresoValido=1;
+    int indiceBusqueda;
+    char auxCodigoPeliculaStr[256];
+    char auxCodigoActorStr[256];
+    char auxValorContratoStr[256];
+    int sePudo=-1;
+    auxCodigoRepartoInt=*contAltas;
+    indiceBusqueda=elenco_buscarPorCodigoDeReparto(listaElenco,tamElenco,-1);
+    if(indiceBusqueda==-1)
+    {
+        printf("\nNo quedan lugares libres\n");
+    }
+    else
+    {
+        pelicula_mostrarLista(listaPelicula,tamPelicula,listaGenero,tamGenero);
+        if(getStrNumerosSinRango("\nIngrese el codigo de una pelicula: ",auxCodigoPeliculaStr,"\nSolo se permiten numeros\n",3))
+        {
+            if(pelicula_buscarPorCodigo(listaPelicula,tamPelicula,atoi(auxCodigoPeliculaStr))==-1)
+            {
+                printf("\nEl id de la pelicula ingresada no existe\n");
+                flagIngresoValido=0;
+            }
+        }
+        else
+        {
+            flagIngresoValido=0;
+        }
+        if(flagIngresoValido==1)
+        {
+            actor_mostrarLista(listaActor,tamActor,listaPais,tamPais);
+            if(getStrNumerosSinRango("\nIngrese el codigo de un actor: ",auxCodigoActorStr,"\nSolo se permiten numeros\n",3))
+            {
+                if(actor_buscarPorCodigo(listaActor,tamActor,atoi(auxCodigoActorStr))==-1)
+                {
+                    printf("\nEl id del actor ingresado no existe\n");
+                    flagIngresoValido=0;
+                }
+                else if(elenco_buscarUnElenco(listaElenco,tamElenco,atoi(auxCodigoActorStr),atoi(auxCodigoPeliculaStr))!=-1)
+                {
+                    printf("\nEl par pelicula actor ingresado ya existe\n");
+                    flagIngresoValido=0;
+                }
+            }
+            else
+            {
+                flagIngresoValido=0;
+            }
+        }
+        if(flagIngresoValido==1)
+        {
+            if(!getStrNumerosFlotantes("\nIngrese el valor del contrato: ",auxValorContratoStr,"\nSolo se permiten numeros y un solo punto\n","\nNumero valido entre el 1000 y el 10000\n",1000,10000,3))
+            {
+                flagIngresoValido=0;
+            }
+        }
+        if(flagIngresoValido==1)
+        {
+            confirmacion=confirmarCambios("\nDesea dar de alta al elenco? (s/n): ","\nSolo confirme con ('s' o con 'n'): ");
+            if(confirmacion==1)
+            {
+                auxDatos.codigoDeReparto=auxCodigoRepartoInt;
+                auxDatos.codigoPelicula=atoi(auxCodigoPeliculaStr);
+                auxDatos.codigoActor=atoi(auxCodigoActorStr);
+                auxDatos.valorContrato=atof(auxValorContratoStr);
+                listaElenco[indiceBusqueda]=auxDatos;
+                *contAltas=auxCodigoRepartoInt+1;
+                printf("\nSe ha dado de alta al codigo numero %d\n",auxCodigoRepartoInt);
+                sePudo=1;
+            }
+            else if(confirmacion==0)
+            {
+                printf("\nAlta cancelada por el usuario\n");
+                sePudo=0;
+            }
+        }
+        if(sePudo==-1)
+        {
+            printf("\nAlta cancelada\n");
+        }
+    }
+    return sePudo;
+}
+int elenco_buscarUnElenco(eElenco listaElenco[],int tamElenco,int codigoActorABuscar,int codigoPeliculaABuscar)
+{
+    int retorno=-1;
+    int i;
+    for(i=0;i<tamElenco;i++)
+    {
+        if((listaElenco[i].codigoDeReparto!=-1) && (listaElenco[i].codigoActor==codigoActorABuscar) && (listaElenco[i].codigoPelicula==codigoPeliculaABuscar))
+        {
+            retorno=i;
+            break;
+        }
+    }
+    return retorno;
+}
+void elenco_ordenarDatos(eElenco listaElenco[],int tamElenco,ePelicula listaPelicula[],int tamPelicula,eActor listaActor[],int tamActor)
+{
+    int indiceBusquedaI;
+    int indiceBusquedaJ;
+    int i;
+    int j;
+    eElenco auxDatos;
+    for(i=0;i<tamElenco-1;i++)
+    {
+        for(j=i+1;j<tamElenco;j++)
+        {
+            indiceBusquedaI=pelicula_buscarPorCodigo(listaPelicula,tamPelicula,listaElenco[i].codigoPelicula);
+            indiceBusquedaJ=pelicula_buscarPorCodigo(listaPelicula,tamPelicula,listaElenco[j].codigoPelicula);
+            if(strcmp(listaPelicula[indiceBusquedaI].descripcion,listaPelicula[indiceBusquedaJ].descripcion)==1)
+            {
+                auxDatos=listaElenco[i];
+                listaElenco[i]=listaElenco[j];
+                listaElenco[j]=auxDatos;
+            }
+            else if(strcmp(listaPelicula[indiceBusquedaI].descripcion,listaPelicula[indiceBusquedaJ].descripcion)==0)
+            {
+                indiceBusquedaI=actor_buscarPorCodigo(listaActor,tamActor,listaElenco[i].codigoActor);
+                indiceBusquedaJ=actor_buscarPorCodigo(listaActor,tamActor,listaElenco[j].codigoActor);
+                if(strcmp(listaActor[indiceBusquedaI].nombre,listaActor[indiceBusquedaJ].nombre)==1)
+                {
+                    auxDatos=listaElenco[i];
+                    listaElenco[i]=listaElenco[j];
+                    listaElenco[j]=auxDatos;
+                }
+            }
+        }
+    }
+}
+void elenco_mostrarPeliculasConActorArgentino(eElenco listaElenco[],int tamElenco,ePelicula listaPelicula[],int tamPelicula,eActor listaActor[],int tamActor,eGenero listaGenero[],int tamGenero,ePais listaPais[],int tamPais)
+{
+    int indiceBusquedaActor;
+    int indiceBusquedaPelicula;
+    int indiceBusquedaGenero;
+    int i;
+    printf("\nCODIGO            DESCRIPCION      DURACION        GENERO      FECHA_ESTRENO\n");
+    for(i=0;i<tamElenco;i++)
+    {
+        indiceBusquedaActor=actor_buscarPorCodigo(listaActor,tamActor,listaElenco[i].codigoActor);
+        indiceBusquedaPelicula=pelicula_buscarPorCodigo(listaPelicula,tamPelicula,listaElenco[i].codigoPelicula);
+        indiceBusquedaGenero=genero_buscarPorId(listaGenero,tamGenero,listaPelicula[indiceBusquedaPelicula].idGenero);
+        if((listaActor[indiceBusquedaActor].idNacionalidad==pais_buscarIdPorDescripcion(listaPais,tamPais,"Argentina")) && (listaActor[indiceBusquedaActor].estado==OCUPADO) && (listaElenco[i].codigoActor==listaActor[indiceBusquedaActor].codigo) && (listaElenco[i].codigoPelicula==listaPelicula[indiceBusquedaPelicula].codigo))
+        {
+            pelicula_mostrarUnoSolo(listaPelicula[indiceBusquedaPelicula],listaGenero,indiceBusquedaGenero);
+        }
+    }
+}
+void elenco_mostrarPeliculasConUnActorSeleccionado(eElenco listaElenco[],int tamElenco,ePelicula listaPelicula[],int tamPelicula,eActor listaActor[],int tamActor,eGenero listaGenero[],int tamGenero,ePais listaPais[],int tamPais)
+{
+    char actorSeleccionado[256];
+    int indiceBusquedaActor;
+    int indiceBusquedaPelicula;
+    int indiceBusquedaGenero;
+    int i;
+    actor_mostrarLista(listaActor,tamActor,listaPais,tamPais);
+    if(getStrNumerosSinRango("\nIngrese el codigo de un actor: ",actorSeleccionado,"\nSolo se permiten numeros\n",3))
+    {
+        if(actor_buscarPorCodigo(listaActor,tamActor,atoi(actorSeleccionado))==-1)
+        {
+            printf("\nEl codigo ingresado no existe\n");
+        }
+        else
+        {
+            printf("\nCODIGO            DESCRIPCION      DURACION        GENERO      FECHA_ESTRENO\n");
+            for(i=0;i<tamElenco;i++)
+            {
+                indiceBusquedaActor=actor_buscarPorCodigo(listaActor,tamActor,listaElenco[i].codigoActor);
+                indiceBusquedaPelicula=pelicula_buscarPorCodigo(listaPelicula,tamPelicula,listaElenco[i].codigoPelicula);
+                indiceBusquedaGenero=genero_buscarPorId(listaGenero,tamGenero,listaPelicula[indiceBusquedaPelicula].idGenero);
+                if((indiceBusquedaActor==actor_buscarPorCodigo(listaActor,tamActor,atoi(actorSeleccionado))) && (listaActor[indiceBusquedaActor].estado==OCUPADO) && (listaElenco[i].codigoActor==listaActor[indiceBusquedaActor].codigo) && (listaElenco[i].codigoPelicula==listaPelicula[indiceBusquedaPelicula].codigo))
+                {
+                    pelicula_mostrarUnoSolo(listaPelicula[indiceBusquedaPelicula],listaGenero,indiceBusquedaGenero);
+                }
+            }
+        }
+    }
+}
+void elenco_mostrarActoresQueNoParticipanEnPeliculas(eElenco listaElenco[],int tamElenco,ePelicula listaPelicula[],int tamPelicula,eActor listaActor[],int tamActor,eGenero listaGenero[],int tamGenero,ePais listaPais[],int tamPais)
+{
+    int indiceBusquedaPais;
+    int indiceBusquedaActor;
+    int i;
+    int j;
+    printf("\nID          NOMBRE        APELLIDO        GENERO          NACIONALIDAD\n");
+    for(i=0;i<tamActor;i++)
+    {
+        for(j=0;j<tamElenco;j++)
+        {
+            indiceBusquedaActor=actor_buscarPorCodigo(listaActor,tamActor,listaElenco[j].codigoActor);
+            indiceBusquedaPais=pais_buscarPorId(listaPais,tamPais,listaActor[i].idNacionalidad);
+            if((listaActor[i].estado==OCUPADO) && (indiceBusquedaActor==-1))
+            {
+                actor_mostrarUnoSolo(listaActor[j],listaPais,indiceBusquedaPais);
+            }
+        }
+    }
+}
---
+++ Clases_De_Repaso/Clase_Repaso_11/Primer_Parcial_Laboratorio_enunciados/main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Validaciones.h"
+#include "Genero.h"
+#include "Pelicula.h"
+#include "Actor.h"
+#include "Elenco.h"
+#include "Pais.h"
+#define MAX_GENERO 4
+#define MAX_PELICULA 5
+#define MAX_ACTOR 100
+#define MAX_PAIS 5
+int main()
+{
+    eActor listaActores[MAX_ACTOR];
+    eGenero listaGeneros[MAX_GENERO];
+    ePelicula listaPeliculas[MAX_PELICULA];
+    eElenco listaElencos[MAX_ACTOR*MAX_PELICULA];
+    ePais listaPaises[MAX_PAIS];
+
+
+    actor_inicializarDatos(listaActores,MAX_ACTOR);
+    elenco_inicializarDatos(listaElencos,MAX_ACTOR*MAX_PELICULA);
+    actor_harcodearDatos(listaActores);
+    genero_harcodearDatos(listaGeneros);
+    pelicula_harcodearDatos(listaPeliculas);
+    elenco_harcodearDatos(listaElencos);
+    pais_harcodearDatos(listaPaises);
+
+    int contAltasActores=6;
+    int contAltasElencos=6;
+
+    int opcionMenu;
+    char continuarMenu='s';
+    do
+    {
+        printf("1-GESTION DE ACTORES\n");
+        printf("2-GESTION DE ELENCOS\n");
+        printf("3-INFORMES\n");
+        printf("4-SALIR\n");
+        opcionMenu=getInt("\nIngrese una opcion: ");
+        switch(opcionMenu)
+        {
+            case 1:
+                actor_gestionarActores(listaActores,MAX_ACTOR,listaPaises,MAX_PAIS,&contAltasActores);
+                break;
+            case 2:
+                elenco_gestionarElencos(listaElencos,MAX_ACTOR*MAX_PELICULA,listaActores,MAX_ACTOR,listaPeliculas,MAX_PELICULA,listaGeneros,MAX_GENERO,listaPaises,MAX_PAIS,&contAltasElencos);
+                break;
+            case 3:
+                elenco_informarDatos(listaElencos,MAX_ACTOR*MAX_PELICULA,listaActores,MAX_ACTOR,listaPeliculas,MAX_PELICULA,listaGeneros,MAX_GENERO,listaPaises,MAX_PAIS);
+                break;
+            case 4:
+                continuarMenu='n';
+                break;
+            default:
+                printf("\nOpcion ingresada no valida\n");
+        }
+        system("pause");
+        system("cls");
+    }
+    while(continuarMenu=='s');
+
+    return 0;
+}
